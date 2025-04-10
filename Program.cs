5029d7d69bf43f31e3774a40341ba950606acac0
GDEUHI <goadane@Icloud.com>
2025-04-10 12:34:00 +0200

    Add Slide 88 loop structure exercises

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("----------------------------------");
+        Console.WriteLine("|       New C# Starter App       |");
+        Console.WriteLine("----------------------------------");
+        Console.ResetColor();
+        Console.WriteLine();
+
+        Console.WriteLine("Hello from your template!");
+    }
+}
